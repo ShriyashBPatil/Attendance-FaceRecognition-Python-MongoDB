38e2d07457c0343f70ad4bdf1451fe517319f684
ShriyashBPatil <115957962+ShriyashBPatil@users.noreply.github.com>
2024-04-11 12:37:58 +0530

    sd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 """
 =================================================================================================
-|Github Repository - https://github.com/ShriyashBPatil/Attendance-FaceRecognition-Python-MongoDB|
+|Github Repository - https://github.com/ShriyashBPatil/Attendance-FaceRecognition-Python-MongoDB |
 |This repository houses an innovative attendance management system developed using Python.       |
 |Leveraging the power of computer vision, DeepFace, and MongoDB, this application simplifies     |
 |the attendance tracking process with just a snap!                                               |
